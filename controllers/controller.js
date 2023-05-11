b9d057fa12c1dc3661e86def827db6028f684545
Convinceo8 <dual.haseo@gmail.com>
2023-05-11 14:24:36 +0700

    Vince Ejs login register

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,24 @@
 class Controller {
 
-static home(req, res){
-    res.send("home")
-}
+    static home(req, res){
+        res.render("home")
+    }
+
+    static registerForm(req, res){
+        res.render("register-form")
+    }
+
+    static postRegister(req,res){
+        res.send("post register")
+    }
+
+    static loginForm(req, res){
+        res.render("login-form")
+    }
+
+    static postLogin(req,res){
+        res.send("post login")
+    }
 }
 
 module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,9 @@ const router = express.Router()
 const Controller = require("../controllers/controller")
 
 router.get('/', Controller.home)
+router.get('/register', Controller.registerForm)
+router.post('/register', Controller.postRegister)
+router.get('/login', Controller.loginForm)
+router.post('/login', Controller.postLogin)
 
 module.exports = router

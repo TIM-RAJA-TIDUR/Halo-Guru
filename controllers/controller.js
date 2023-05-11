896480af383f7eda55a9e50e4ac62e50c076ca32
tengkurizki13 <trizkidinelvipratama@gmail.com>
2023-05-11 16:53:50 +0700

    menambah logout

--- controllers/controller.js
+++ controllers/controller.js
@@ -59,6 +59,16 @@ class Controller {
             res.redirect(`/login?errors=${err}`)
         })
     }
+
+    static logout(req, res){
+        req.session.destroy(err => {
+            if (err) {
+                console.log(err);
+            }else{
+                res.redirect("/login")
+            }
+        })
+    }
 }
 
 module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/register', Controller.registerForm)
 router.post('/register', Controller.postRegister)
 router.get('/login', Controller.loginForm)
 router.post('/login', Controller.postLogin)
+router.post('/logout', Controller.logout)
 
 
 router.use(isLogin)
